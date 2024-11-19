9524ea6b2fba08d9e0367f99fc7a475810cffc30
AaronvonMaekel <aaronmaekel@web.de>
2024-11-19 20:06:48 +0100

    added assigment 5 exercise  2 data

--- task5_2.py
+++ task5_2.py
@@ -12,7 +12,7 @@ def kappa(
     q: float,
     omegas: np.array,
 ) -> float:
-    kappa = 0
+    kappa = np.zeros(len(x))
     for j in range(1, len(omegas) + 1):
         # Compute corresponding basis function.
         a = (1 / j ** q) * np.sin(2 * j * np.pi * x)
@@ -28,14 +28,20 @@ def create_stiffness_matrix(
 ):
     h = 1 / (N - 1)
     diagonals = [np.empty((N - 1)), np.empty((N)), np.empty((N - 1))]
-    
+    TRAPEZ_POINTS = 21
     for i in range(len(diagonals[1])):
-        diagonals[1][i] = 1/(h**2) * integrate.quad(lambda x: kappa(x, q, omegas), h*(i-1), h*(i+1))[0]
-    
+        #diagonals[1][i] = 1/(h**2) * integrate.quad(lambda x: kappa(x, q, omegas), h*(i-1), h*(i+1))[0]
+        x = np.linspace(h*(i-1), h*(i+1),TRAPEZ_POINTS)
+        y = kappa(x, q,  omegas)
+        diagonals[1][i]  =    np.trapz(y, x)
     for i in range(len(diagonals[0])):
-        diagonals[0][i] = -1/(h**2) * integrate.quad(lambda x: kappa(x, q, omegas), h*(i), h*(i+1))[0]
-        diagonals[2][i] = -1/(h**2) * integrate.quad(lambda x: kappa(x, s, omegas), h*(i), h*(i+1))[0]
-    
+        #diagonals[0][i] = -1/(h**2) * integrate.quad(lambda x: kappa(x, q, omegas), h*(i), h*(i+1))[0]
+       
+        x = np.linspace( h*(i), h*(i+1),TRAPEZ_POINTS)
+        y = kappa(x, q, omegas)
+        diagonals[0][i]  =    np.trapz(y, x)
+
+        diagonals[2][i] = diagonals[0][i]
     # Impose boundary conditions.
     diagonals[0][0] = 0
     diagonals[2][0] = 0
@@ -49,12 +55,14 @@ def create_stiffness_matrix(
 
 def create_rhs(N: int, f):
     h = 1 / (N - 1)
-
+    TRAPEZ_POINTS = 21
     vec = np.empty((N))
 
     for i in range(len(vec)):
-        vec[i] = integrate.quad(lambda x: f(x)*(1-abs(h*i-x)/h), h*(i-1), h*(i+1))[0]
-
+        #vec[i] = integrate.quad(lambda x: f(x)*(1-abs(h*i-x)/h), h*(i-1), h*(i+1))[0]
+        x = np.linspace(h*(i-1), h*(i+1),TRAPEZ_POINTS)
+        y = f(x)*(1-abs(h*i-x)/h)
+        vec[i] =    np.trapz(y, x)
     # Impose boundary conditions.
     vec[0] = 0
     vec[-1] = 0
@@ -62,27 +70,74 @@ def create_rhs(N: int, f):
     return vec
 
 
+
 if __name__ == "__main__":
-    N = 1001
+    h=0.01
+    
+    N = int(1/h) +1 
     Nmesh = np.linspace(0,1,N)
     
-    N_samples = 10
 
     s = 10
-    q = 2
-
-    f = create_rhs(N, lambda x: 1)
-    
-    MC_estimate = 0
+    #########c
+    #levels = np.logspace(2,4.5,6,dtype=np.integer)
+    #f = create_rhs(N, lambda x: 1)
+    #Qs = np.array([1.1,2,10])
+    #MC_estimate = 0
     # Perform vanilla MC.
-    for _ in tqdm(range(N_samples)):
-        omegas = random.standard_normal(s)
-        mat = create_stiffness_matrix(N, q, omegas)
+    #MC_var  = np.empty((len(Qs),len(levels)))
+    #i = 0
+    #k = 0
+    #for q in Qs:
+    #    i = 0
+    #    for N_samples in levels:
+    #        MC_samples = np.empty(N_samples)
+    #        for j in tqdm(range(N_samples)):
+    #            omegas = random.standard_normal(s)
+    #            mat = create_stiffness_matrix(N, q, omegas)
 
-        u = sp.sparse.linalg.spsolve(mat, f)
+    #            u = sp.sparse.linalg.spsolve(mat, f)
 
-        MC_estimate += u[int((N - 1) * 0.7)]
-    MC_estimate /= N_samples
-    
-    print(f"MC estimate: {MC_estimate}")
-    
+    #            MC_estimate += u[int((N - 1) * 0.7)]
+    #            MC_samples[j] = u[int((N - 1) * 0.7)]
+    #        MC_var[k,i] = np.var(MC_samples,ddof=1)    
+    #        MC_estimate /= N_samples
+    #        print(f"MC estimate: {MC_estimate}")
+    #        i += 1
+    #    k+= 1
+
+    #
+    ###print(f"MC variance: {MC_var}")
+    #plt.plot(levels,MC_var[0],color="black",label="q=1.1")
+    #plt.plot(levels,MC_var[1],color="blue",label="q=2")
+    #plt.plot(levels,MC_var[2],color="red",label="q=10")
+    #plt.legend()
+    #plt.yscale("log")
+    #plt.xscale("log")
+    #plt.show()
+    #######################d
+    q = 2
+    hmesh = 0.01*np.logspace(-0,-5,10,base=2)
+    MC_mean  = np.empty((len(hmesh))) 
+    N_samples = 1000
+    print(hmesh)
+    i = 0
+    MC_samples = np.empty(N_samples)
+    for h in hmesh:
+        N = int(1/h) +1 
+        Nmesh = np.linspace(0,1,N)
+        f = create_rhs(N, lambda x: 1)                    
+        for j in tqdm(range(N_samples)):
+            omegas = random.standard_normal(s)
+            mat = create_stiffness_matrix(N, q, omegas)
+
+            u = sp.sparse.linalg.spsolve(mat, f)
+            MC_samples[j] = u[int((N - 1) * 0.7)]
+        MC_mean[i] = np.mean(MC_samples)
+        print(f"MC estimate: {MC_mean}")
+        i+=1
+    plt.plot(hmesh, abs(MC_mean[:-1] - MC_mean[-1]),color="black",label="error")
+
+    plt.yscale("log")
+    plt.xscale("log")
+    plt.show()
